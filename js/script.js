ae47bb0b06dee2bbfbe5d69dcda07c79333e1d69
Erik Marques <65323066+imerik1@users.noreply.github.com>
2020-11-15 21:00:21 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+validatePermission()
+
+function validatePermission() {
+    if (stash.get('permission')) {
+        disableBox()
+    }
+}
+
+function savePref() {
+    let check = document.getElementById('check')
+    if (check.checked) {
+        stash.set('permission', true);
+        stash.set('contador', 0)
+        disableBox()
+    }
+}
+
+function disableBox() {
+    let cookiesPermission = document.getElementById('cookies')
+    cookiesPermission.style.display = 'none';
+    $( "#root" ).load( "./contador.html" );
+}
